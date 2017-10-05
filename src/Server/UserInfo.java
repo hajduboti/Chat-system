1daac7e7696a08e2fa2602776dfd5d0f88f87e6d
hajduboti <boto0018@stud.kea.dk>
2017-10-05 14:46:17 +0200

    SECOND

---
+++ src/Server/UserInfo.java
@@ -0,0 +1,14 @@
+package Server;
+
+public class UserInfo {
+
+    String name;
+    String  IP;
+    int port;
+
+    public UserInfo(String name, String IP, int port){
+        this.name = name;
+        this.IP= IP;
+        this.port = port;
+    }
+}
